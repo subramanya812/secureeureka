5cb9d478f1ca8e18be19773df5aeefc3efb79b83
subramanya812 <sunil.subramanya812@gmail.com>
2019-08-14 16:39:47 +0530

    configuration file

--- src/main/java/discoveryservice/eurekaservice/EurekaServiceApplication.java
+++ src/main/java/discoveryservice/eurekaservice/EurekaServiceApplication.java
@@ -1,8 +1,13 @@
 package discoveryservice.eurekaservice;
 
+import org.apache.catalina.Context;
+import org.apache.catalina.connector.Connector;
+import org.apache.tomcat.util.descriptor.web.SecurityCollection;
+import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableEurekaServer
